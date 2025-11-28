b6270b284c46b87794e2528002880d59b8b03201
nodino <arnaud.mansat@gmail.com>
2025-11-28 18:16:32 +0100

    Add French translation file for plugin Viz

---
+++ locales/fr_FR.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file viz/locales/fr_FR.php
+ * @brief French translation file
+ */
+
+$LANG['plugin_viz'] = [
+   // Configuration
+   'Configuration du plugin Viz' => 'Configuration du plugin Viz',
+   'Configuration Viz' => 'Configuration Viz',
+   'URL du site à intégrer' => 'URL du site à intégrer',
+   'Sauvegarder' => 'Sauvegarder',
+   'Configuration sauvegardée avec succès' => 'Configuration sauvegardée avec succès',
+   'Erreur lors de la sauvegarde de la configuration' => 'Erreur lors de la sauvegarde de la configuration',
+   'URL invalide. Veuillez saisir une URL valide.' => 'URL invalide. Veuillez saisir une URL valide.',
+   
+   // Menu
+   'Viz - Site Web' => 'Viz - Site Web',
+   'Voir le site' => 'Voir le site',
+   'Site Web Externe' => 'Site Web Externe',
+   
+   // Messages
+   'Configuration requise' => 'Configuration requise',
+   'L\'URL du site web n\'est pas configurée.' => 'L\'URL du site web n\'est pas configurée.',
+   'Configurer maintenant' => 'Configurer maintenant',
+   'Veuillez contacter un administrateur pour configurer le plugin.' => 'Veuillez contacter un administrateur pour configurer le plugin.',
+   'Erreur de configuration' => 'Erreur de configuration',
+   'L\'URL configurée est invalide.' => 'L\'URL configurée est invalide.',
+   'Attention : certains sites bloquent l\'affichage en iframe (X-Frame-Options header)' => 'Attention : certains sites bloquent l\'affichage en iframe (X-Frame-Options header)',
+   'Si le site ne s\'affiche pas, il est possible qu\'il bloque l\'intégration en iframe (politique X-Frame-Options).' => 'Si le site ne s\'affiche pas, il est possible qu\'il bloque l\'intégration en iframe (politique X-Frame-Options).',
+];
+
+return $LANG['plugin_viz'];
+?>
